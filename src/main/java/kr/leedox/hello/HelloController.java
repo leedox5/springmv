dfd187963a8b69481621d6a683d273a17295c503
이명호 <leedox@naver.com>
2021-12-23 09:26:31 +0900

    [2021.12.23] HELLO

---
+++ src/main/java/kr/leedox/hello/HelloController.java
@@ -0,0 +1,14 @@
+package kr.leedox.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Spring Boot and Maven!!!";
+    }
+
+}
