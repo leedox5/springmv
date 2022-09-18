5a070ddf592b7cd3480d45f1b5702315b351a3ae
Myungho Lee <leedox@naver.com>
2022-09-19 08:35:38 +0900

    SYNC

--- src/main/java/kr/leedox/entity/Member.java
+++ src/main/java/kr/leedox/entity/Member.java
@@ -1,49 +1,5 @@
 package kr.leedox.entity;
 
-<<<<<<< HEAD
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity(name = "member")
-public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
-    @Column(nullable = false, unique = true, length = 50)
-    private String userId;
-
-    @Column(nullable = false, length = 100)
-    private String name;
-
-    public int getId() {
-        return this.id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUserId() {
-        return this.userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    
-=======
 import lombok.*;
 
 import javax.persistence.*;
@@ -64,5 +20,4 @@ public class Member {
     private String name;
     private String password;
 
->>>>>>> fa9d3e5c47e552a7b04961f1c8a03284c770f892
 }
