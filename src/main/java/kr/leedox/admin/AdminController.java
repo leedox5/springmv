05924a410c37b162d6e5ac2e4634203dfae74ba3
이명호 <leedox@naver.com>
2022-09-23 13:46:11 +0900

    ADMIN

--- src/main/java/kr/leedox/admin/AdminController.java
+++ src/main/java/kr/leedox/admin/AdminController.java
@@ -1,9 +1,11 @@
 package kr.leedox.admin;
 
 import kr.leedox.entity.Wordbook;
+import kr.leedox.service.MemberAdapter;
 import kr.leedox.service.WordService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +22,11 @@ public class AdminController {
     WordService wordService;
 
     @GetMapping("/wordbook")
-    public String wordbook(Model model) {
+    public String wordbook(Model model, @AuthenticationPrincipal MemberAdapter author) {
         List<Wordbook> wordbooks = wordService.getListAll();
+        model.addAttribute("list", wordbooks);
+        model.addAttribute("path", "eng");
+        model.addAttribute(("author", author.getMember()));
+        return "thymeleaf/word_list";
     }
 }
