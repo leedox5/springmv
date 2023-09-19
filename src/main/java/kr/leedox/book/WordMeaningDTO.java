a71ad4e7fec6d521f65c4fb964cc2679079083c9
Myungho Lee <leedox@naver.com>
2023-09-19 23:30:34 +0900

    PayPal 적용

--- src/main/java/kr/leedox/book/WordMeaningDTO.java
+++ src/main/java/kr/leedox/book/WordMeaningDTO.java
@@ -3,10 +3,13 @@ package kr.leedox.book;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Data
 public class WordMeaningDTO implements Serializable {
+    @NotEmpty(message = "내용은 필수 항목 입니다.")
     private String meaning;
     @JsonProperty("wordbook_id")
     private int wordbookId;
